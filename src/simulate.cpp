a76438dc74a2b88fc607dc6b1ecc8d743cd65b47
Mark T. Holder <mtholder@gmail.com>
2023-08-09 16:13:16 -0500

    rng stuff

--- src/simulate.cpp
+++ src/simulate.cpp
@@ -3,6 +3,7 @@
 #include <g3log/loglevels.hpp>
 #include <vector>
 #include <iostream>
+#include <random>
 #include "error.h"
 namespace po = boost::program_options;
 using po::variables_map;
@@ -24,6 +25,7 @@ variables_map parse_cmd_line(int argc, char* argv[]) {
             value<int_vec_t>()->multitoken(), 
             "4 integers for the number of counts to be simulated for A, B, C, and D" )
         ("num-sites", value<int>(), "The number of PoMo sites to simulate" )
+        ("seed", value<int>(), "The seed for the RNG" )
         ;
     options_description model("Model options");
     visible.add(data).add(model).add(standard_options());
@@ -44,10 +46,12 @@ class SimOptions {
 public:
     int_vec_t sample_counts;
     int num_sites;
+    unsigned int single_seed;
+    mt19937 rng;
+
 };
 
-inline SimOptions parse_and_validate_cmd_line(int argc, char * argv[]) {
-    SimOptions so;
+inline void parse_and_validate_cmd_line(int argc, char * argv[], SimOptions & so) {
     const auto args = parse_cmd_line(argc, argv);
     try {
         so.sample_counts = args["sample-counts"].as<int_vec_t>();
@@ -70,14 +74,33 @@ inline SimOptions parse_and_validate_cmd_line(int argc, char * argv[]) {
     if (so.num_sites < 1) {
         throw  OTCError() << "num-sites must be a positive integer.";
     }
-    
-    return so;
+    bool seed_sent = true;
+    try {
+        so.single_seed = args["seed"].as<int>();
+    } catch (...) {
+        seed_sent = false;
+        so.single_seed = 0; 
+    }
+    if (seed_sent) {
+        if (so.single_seed < 1) {
+            throw  OTCError() << "seed (if provided) must be a positive integer.";
+        }
+        so.rng.seed(so.single_seed);
+    } else {
+        // random seeding code from https://www.phy.olemiss.edu/~kbeach/guide/2020/01/11/random/
+        std::array<int,624> seed_data;
+        std::random_device r;
+        std::generate_n(seed_data.data(), seed_data.size(), std::ref(r));
+        seed_seq seeds{std::begin(seed_data), std::end(seed_data)};
+        so.rng.seed(seeds);
+    }
+
 }
 
 int main(int argc, char *argv[]) {
 	SimOptions options;
     try {
-        options = parse_and_validate_cmd_line(argc, argv);
+        parse_and_validate_cmd_line(argc, argv, options);
     } catch (std::exception& e) {
         std::cerr << "simulate-pomo-introgression: Error! " << e.what() << std::endl;
         exit(1);
@@ -87,6 +110,11 @@ int main(int argc, char *argv[]) {
     std::cerr << "  " << options.sample_counts.at(1) << " seqs from B\n";
     std::cerr << "  " << options.sample_counts.at(2) << " seqs from C\n";
     std::cerr << "  " << options.sample_counts.at(3) << " seqs from D\n";
+    if (options.single_seed > 0) {
+        std::cerr << "RNG seed was " << options.single_seed << "\n";
+    } else {
+        std::cerr << "RNG seeded from random device\n";
+    }
     
 	return 0;
 }
