0b67ef24373fc36b83d42c20a176f5c1ebfbdfae
Mark T. Holder <mtholder@gmail.com>
2023-08-11 12:06:59 -0500

    cli

--- src/simulate.cpp
+++ src/simulate.cpp
@@ -12,6 +12,7 @@ namespace fs = std::filesystem;
 using otc::OTCError;
 
 using int_vec_t = vector<int>;
+using double_vec_t = vector<double>;
 
 variables_map parse_cmd_line(int argc, char* argv[]) {
     using namespace po;
@@ -28,6 +29,18 @@ variables_map parse_cmd_line(int argc, char* argv[]) {
         ("seed", value<int>(), "The seed for the RNG" )
         ;
     options_description model("Model options");
+    model.add_options()
+        ("base-freqs", 
+            value<double_vec_t>()->multitoken(), 
+            "3 floating point numbers for the base frequencies: pi_A, pi_C, and pi_G." )
+        ("r-mat",
+            value<double_vec_t>()->multitoken(),
+            "6 floating point numbers for the exchangeabilities: r_AC, r_AG, r_AT, r_CG, r_CT, and r_GT." )
+        ("pop-size,N",
+            value<int>()->multitoken(),
+            "N, the population size of the PoMo state space" )
+        ;
+    
     visible.add(data).add(model).add(standard_options());
     
     variables_map vm = parse_cmd_line_standard(argc,
@@ -48,11 +61,14 @@ public:
     int num_sites;
     unsigned int single_seed;
     mt19937 rng;
-
+    vector<double> base_freqs;
+    vector<double> r_mat;
+    int pomo_pop_size;
 };
 
 inline void parse_and_validate_cmd_line(int argc, char * argv[], SimOptions & so) {
     const auto args = parse_cmd_line(argc, argv);
+    // sample counts
     try {
         so.sample_counts = args["sample-counts"].as<int_vec_t>();
     } catch (...) {
@@ -66,6 +82,7 @@ inline void parse_and_validate_cmd_line(int argc, char * argv[], SimOptions & so
             throw OTCError() << "sample-counts should be followed by non-negative integers '" << c << "' found.";
         }
     }
+    // number of sites
     try {
         so.num_sites = args["num-sites"].as<int>();
     } catch (...) {
@@ -74,6 +91,7 @@ inline void parse_and_validate_cmd_line(int argc, char * argv[], SimOptions & so
     if (so.num_sites < 1) {
         throw  OTCError() << "num-sites must be a positive integer.";
     }
+    // RNG seeding
     bool seed_sent = true;
     try {
         so.single_seed = args["seed"].as<int>();
@@ -94,7 +112,74 @@ inline void parse_and_validate_cmd_line(int argc, char * argv[], SimOptions & so
         seed_seq seeds{std::begin(seed_data), std::end(seed_data)};
         so.rng.seed(seeds);
     }
+    // base frequencies
+    try {
+        so.base_freqs = args["base-freqs"].as<double_vec_t>();
+    } catch (...) {
+        so.base_freqs = double_vec_t{0.25, 0.25, 0.25};
+    }
+    if (so.base_freqs.size() != 3) {
+        throw  OTCError() << "base-freqs should be followed by 3 floating point numbers.";
+    }
+    double sumbf = 0.0;
+    for (auto c : so.base_freqs) {
+        if (c <= 0.0 || c >= 1.0) {
+            throw OTCError() << "base-freqs must be in the range (0, 1). '" << c << "' found.";
+        }
+        sumbf += c;
+    }
+    if (sumbf >= 1.0) {
+        throw OTCError() << "the 3 base-freqs must be sum to less than 1. Sum '" << sumbf << "' found.";
+    }
+    so.base_freqs.push_back(1.0 - sumbf);
+    // r-mat
+    try {
+        so.r_mat = args["r-mat"].as<double_vec_t>();
+    } catch (...) {
+        so.r_mat = double_vec_t{1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+    }
+    if (so.r_mat.size() != 6) {
+        throw  OTCError() << "r-mat should be followed by 6 floating point numbers.";
+    }
+    for (auto c : so.r_mat) {
+        if (c <= 0.0) {
+            throw OTCError() << "r-mat must be > 0, but '" << c << "' found.";
+        }
+    }
+    // N of PoMo model
+    try {
+        so.pomo_pop_size = args["pop-size"].as<int>();
+    } catch (...) {
+        so.pomo_pop_size = 4;
+    }
+    if (so.pomo_pop_size < 2) {
+        throw  OTCError() << "num-sites must be greater than 1.";
+    }
+}
 
+ostream & operator<<(ostream & out, const SimOptions & options) {
+    out << "To simulate " << options.num_sites << " sites with:\n";
+    out << "  " << options.sample_counts.at(0) << " seqs from A\n";
+    out << "  " << options.sample_counts.at(1) << " seqs from B\n";
+    out << "  " << options.sample_counts.at(2) << " seqs from C\n";
+    out << "  " << options.sample_counts.at(3) << " seqs from D\n";
+    if (options.single_seed > 0) {
+        out << "RNG seed was " << options.single_seed << "\n";
+    } else {
+        out << "RNG seeded from random device\n";
+    }
+    out << "PoMo population size, N = " << options.pomo_pop_size << "\n";
+    out << "Mutational base frequencies (A, C, G, T) = (";
+    for (auto bf : options.base_freqs) {
+        out << bf << ", ";
+    }
+    out << ")\n";
+    out << "Mutational exchangeabilities = (";
+    for (auto rp : options.r_mat) {
+        out << rp << ", ";
+    }
+    out << ")\n";  
+    return out;
 }
 
 int main(int argc, char *argv[]) {
@@ -105,16 +190,6 @@ int main(int argc, char *argv[]) {
         std::cerr << "simulate-pomo-introgression: Error! " << e.what() << std::endl;
         exit(1);
     }
-    std::cerr << "To simulate " << options.num_sites << " sites with:\n";
-    std::cerr << "  " << options.sample_counts.at(0) << " seqs from A\n";
-    std::cerr << "  " << options.sample_counts.at(1) << " seqs from B\n";
-    std::cerr << "  " << options.sample_counts.at(2) << " seqs from C\n";
-    std::cerr << "  " << options.sample_counts.at(3) << " seqs from D\n";
-    if (options.single_seed > 0) {
-        std::cerr << "RNG seed was " << options.single_seed << "\n";
-    } else {
-        std::cerr << "RNG seeded from random device\n";
-    }
-    
+    std::cerr << options;
 	return 0;
 }
